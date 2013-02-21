8c6e1e58b0b37f1f7f30484153c51e9456e1b577
Eric Tobin <eatobin@gmail.com>
2013-02-21 15:10:15 -0600

    initial commit

---
+++ src/main/person.rb
@@ -0,0 +1,13 @@
+class Person
+
+  attr_accessor :name, :maximum_books
+
+  def initialize
+    @name = 'unknown name'
+    @maximum_books = 3
+  end
+
+  def to_s
+    self.name + ' (' + self.maximum_books.to_s + ' books)'
+  end
+end
---
+++ src/test/person_test.rb
@@ -0,0 +1,48 @@
+require_relative('../main/person')
+require 'test/unit'
+
+class PersonTest < Test::Unit::TestCase
+
+  # Called before every test method runs. Can be used
+  # to set up fixture information.
+  def setup
+    #puts 'At the beginning'
+  end
+
+  # Called after every test method runs. Can be used to tear
+  # down fixture information.
+
+  def teardown
+    #puts 'At the end'
+  end
+
+  # Test person
+  def test_person
+    p1 = Person.new
+    assert_equal('unknown name', p1.name)
+    assert_equal(3, p1.maximum_books)
+  end
+
+  # Test person set name
+  def test_person_set_name
+    p2 = Person.new
+    p2.name = 'Fred'
+    assert_equal('Fred', p2.name)
+  end
+
+  # Test person set maximum books
+  def test_person_set_maximum_books
+    p3 = Person.new
+    p3.maximum_books = 66
+    assert_equal(66, p3.maximum_books)
+  end
+
+  # Test person return to_s
+  def test_person_return_to_s
+    p4 = Person.new
+    p4.name = 'Fred Flintstone'
+    p4.maximum_books = 7
+    test_string = 'Fred Flintstone (7 books)'
+    assert_equal(test_string, p4.to_s)
+  end
+end
