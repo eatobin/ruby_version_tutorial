87ef714ade1976dba0e6204c142b6098fff9ebc3
Eric Tobin <eatobin@gmail.com>
2013-02-23 14:47:57 -0600

    delete comments in test

--- src/test/book_test.rb
+++ src/test/book_test.rb
@@ -3,14 +3,12 @@ require 'test/unit'
 
 class BookTest < Test::Unit::TestCase
 
-  # Test book
   def test_book
     b1 = Book.new('Great Expectations')
     assert_equal('Great Expectations', b1.title)
     assert_equal('unknown author', b1.author)
   end
 
-  # Test set new author
   def test_book_new_author
     b2 = Book.new('War And Peace')
     b2.author = 'Eric Tobin'
--- src/test/person_test.rb
+++ src/test/person_test.rb
@@ -3,28 +3,24 @@ require 'test/unit'
 
 class PersonTest < Test::Unit::TestCase
 
-  # Test person
   def test_person
     p1 = Person.new
     assert_equal('unknown name', p1.name)
     assert_equal(3, p1.maximum_books)
   end
 
-  # Test person set name
   def test_person_set_name
     p2 = Person.new
     p2.name = 'Fred'
     assert_equal('Fred', p2.name)
   end
 
-  # Test person set maximum books
   def test_person_set_maximum_books
     p3 = Person.new
     p3.maximum_books = 66
     assert_equal(66, p3.maximum_books)
   end
 
-  # Test person return to_s
   def test_person_return_to_s
     p4 = Person.new
     p4.name = 'Fred Flintstone'
