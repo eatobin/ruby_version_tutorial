8c3b62cf7556ae1f780b5e4487ee95d1e61f9862
Eric Tobin <eatobin@gmail.com>
2013-02-26 21:37:16 -0600

    fixed the test setup!

--- src/test/my_library_test.rb
+++ src/test/my_library_test.rb
@@ -4,56 +4,55 @@ require_relative('../main/person')
 require 'test/unit'
 
 class MyLibraryTest < Test::Unit::TestCase
+  def setup
+    @@b1 = Book.new('Book1')
+    @@b2 = Book.new('Book2')
+    @@b3 = Book.new('Book3')
+    @@p1 = Person.new
+    @@p2 = Person.new
+    @@p1.name = 'Fred'
+    @@p2.name = 'Sue'
+    @@ml = My_library.new('Test Library')
+  end
+
   def test_my_library
-    ml = My_library.new('Test Library')
-    assert_equal('Test Library', ml.name)
-    assert_true(ml.books.instance_of?(Array))
-    assert_true(ml.people.instance_of?(Array))
+    assert_equal('Test Library', @@ml.name)
+    assert_true(@@ml.books.instance_of?(Array))
+    assert_true(@@ml.people.instance_of?(Array))
   end
 
   def test_add_and_delete_book
-    b1 = Book.new('Book1')
-    b2 = Book.new('Book2')
-    b3 = Book.new('Book3')
-    ml = My_library.new('Test Library')
+    assert_equal(0, @@ml.books.length)
 
-    assert_equal(0, ml.books.length)
+    @@ml.add_book(@@b1)
+    @@ml.add_book(@@b2)
+    assert_equal(2, @@ml.books.length)
+    assert_equal(0, @@ml.books.index(@@b1))
+    assert_equal(1, @@ml.books.index(@@b2))
 
-    ml.add_book(b1)
-    ml.add_book(b2)
-    assert_equal(2, ml.books.length)
-    assert_equal(0, ml.books.index(b1))
-    assert_equal(1, ml.books.index(b2))
+    # Try to add a duplicate book (@@b1) - should reject
+    @@ml.add_book(@@b1)
+    assert_equal(2, @@ml.books.length)
 
-    # Try to add a duplicate book (b1) - should reject
-    ml.add_book(b1)
-    assert_equal(2, ml.books.length)
+    @@ml.delete_book(@@b1)
+    assert_equal(1, @@ml.books.length)
+    assert_equal(0, @@ml.books.index(@@b2))
 
-    ml.delete_book(b1)
-    assert_equal(1, ml.books.length)
-    assert_equal(0, ml.books.index(b2))
+    # Try to delete a never-added book (@@b3) - should reject
+    @@ml.delete_book(@@b3)
+    assert_equal(1, @@ml.books.length)
 
-    # Try to delete a never-added book (b3) - should reject
-    ml.delete_book(b3)
-    assert_equal(1, ml.books.length)
-
-    ml.delete_book(b2)
-    assert_equal(0, ml.books.length)
+    @@ml.delete_book(@@b2)
+    assert_equal(0, @@ml.books.length)
   end
 
   def test_add_person
-    p1 = Person.new
-    p2 = Person.new
-    p1.name = 'Fred'
-    p2.name = 'Sue'
-    ml = My_library.new('Test Library')
+    @@ml.add_person(@@p1)
+    @@ml.add_person(@@p2)
+    assert_equal(2, @@ml.people.length)
 
-    ml.add_person(p1)
-    ml.add_person(p2)
-    assert_equal(2, ml.people.length)
-
-    # Try to add a duplicate person (p1) - should reject
-    ml.add_person(p1)
-    assert_equal(2, ml.people.length)
+    # Try to add a duplicate person (@@p1) - should reject
+    @@ml.add_person(@@p1)
+    assert_equal(2, @@ml.people.length)
   end
 end
