946ec8836472ff96d0f59fd6f23b89baf0a01c4d
Eric Tobin <eatobin@gmail.com>
2013-03-05 17:25:19 -0600

    progress commit

--- src/main/my_utilities.rb
+++ src/main/my_utilities.rb
@@ -1,13 +1,21 @@
 class My_utilities
   def self.get_string_from_file(file_name)
-    f = File.open(file_name, 'r')
-    f.read
+    begin
+      f = File.open(file_name, 'r')
+      f.read
+    rescue
+      puts 'ERROR: File did not read.'
+    end
   end
 
   def self.save_string_to_file(file_name, saved_string)
-    f = File.open(file_name, 'w')
-    f.puts saved_string
-    f.close
-    true
+    begin
+      f = File.open(file_name, 'w')
+      f.puts saved_string
+      f.close
+      true
+    rescue
+      puts 'ERROR: File did not write.'
+    end
   end
 end
--- src/test/my_utilities_test.rb
+++ src/test/my_utilities_test.rb
@@ -5,29 +5,24 @@ require_relative('../main/person')
 require 'test/unit'
 
 class MyUtilitiesTest < Test::Unit::TestCase
-
-  # Called before every test method runs. Can be used
-  # to set up fixture information.
-  def setup
-    # Do nothing
-  end
-
-  # Called after every test method runs. Can be used to tear
-  # down fixture information.
-
-  def teardown
-    # Do nothing
-  end
-
   def test_get_string_from_file
     get_file = '../../testgetstring.txt'
     get_string = "testing one, two\nthree\n"
     assert_equal(get_string, My_utilities.get_string_from_file(get_file))
+
+    # This should pass wth rescue error - file NOT read:
+    get_file = '../../testgetstringX.txt'
+    assert_nil(My_utilities.get_string_from_file(get_file))
   end
 
   def test_save_string_to_file
     save_file = '../../testsavestring.txt'
     save_string = "this is test line one\nthis is test line two\n"
     assert_true(My_utilities.save_string_to_file(save_file, save_string))
+    assert_equal(save_string, My_utilities.get_string_from_file(save_file))
+
+    # This should pass wth rescue error - file NOT written:
+    save_file = '../../testsavestring/.txt'
+    assert_nil(My_utilities.save_string_to_file(save_file, save_string))
   end
 end
