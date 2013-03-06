e1190d8a6c7b603725cf50a15861d6b372707fdf
Eric Tobin <eatobin@gmail.com>
2013-03-05 22:20:24 -0600

    progress commit

--- src/main/my_utilities.rb
+++ src/main/my_utilities.rb
@@ -1,3 +1,5 @@
+require 'yaml'
+
 class My_utilities
   def self.get_string_from_file(file_name)
     begin
@@ -17,5 +19,21 @@ class My_utilities
     rescue
       puts 'ERROR: File did not write.'
     end
+  end
+
+  def self.convert_to_yaml(library_object)
+    YAML::dump(library_object)
+  end
+
+  def self.convert_from_yaml(yaml_string)
+    YAML::load(yaml_string)
+  end
+
+  def self.save_my_library_to_yaml_file(file_name, my_library)
+    self.save_string_to_file(file_name, self.convert_to_yaml(my_library))
+  end
+
+  def self.get_my_library_from_yaml_file(file_name)
+    self.convert_from_yaml(self.get_string_from_file(file_name))
   end
 end
