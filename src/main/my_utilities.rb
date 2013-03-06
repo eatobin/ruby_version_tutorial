f1b43d91d18d767924ba90dc3f3fb226c2d5c38b
Eric Tobin <eatobin@gmail.com>
2013-03-06 14:08:40 -0600

    progress

--- src/main/my_utilities.rb
+++ src/main/my_utilities.rb
@@ -1,26 +1,6 @@
 require 'yaml'
 
 class My_utilities
-  def self.get_string_from_file(file_name)
-    begin
-      f = File.open(file_name, 'r')
-      f.read
-    rescue
-      puts 'ERROR: File did not read.'
-    end
-  end
-
-  def self.save_string_to_file(file_name, saved_string)
-    begin
-      f = File.open(file_name, 'w')
-      f.puts saved_string
-      f.close
-      true
-    rescue
-      puts 'ERROR: File did not write.'
-    end
-  end
-
   def self.convert_to_yaml(file_name, library_object)
     begin
       f = File.open(file_name, 'w')
@@ -42,12 +22,4 @@ class My_utilities
       puts 'ERROR: File did not read.'
     end
   end
-
-  #def self.save_my_library_to_yaml_file(file_name, my_library)
-  #  self.save_string_to_file(file_name, self.convert_to_yaml(my_library))
-  #end
-  #
-  #def self.get_my_library_from_yaml_file(file_name)
-  #  self.convert_from_yaml(self.get_string_from_file(file_name))
-  #end
 end
--- src/test/my_utilities_test.rb
+++ src/test/my_utilities_test.rb
@@ -5,28 +5,7 @@ require_relative('../main/person')
 require 'test/unit'
 
 class MyUtilitiesTest < Test::Unit::TestCase
-  def test_get_string_from_file
-    get_file = '../../testgetstring.txt'
-    get_string = "testing one, two\nthree\n"
-    assert_equal(get_string, My_utilities.get_string_from_file(get_file))
-
-    # This should pass wth rescue error - file NOT read:
-    get_file = '../../testgetstringX.txt'
-    assert_nil(My_utilities.get_string_from_file(get_file))
-  end
-
-  def test_save_string_to_file
-    save_file = '../../testsavestring.txt'
-    save_string = "this is test line one\nthis is test line two\n"
-    assert_true(My_utilities.save_string_to_file(save_file, save_string))
-    assert_equal(save_string, My_utilities.get_string_from_file(save_file))
-
-    # This should pass wth rescue error - file NOT written:
-    save_file = '../../testsavestring/.txt'
-    assert_nil(My_utilities.save_string_to_file(save_file, save_string))
-  end
-
-  def test_convert_to_yaml
+  def test_1_convert_to_yaml
     save_file = '../../saveyaml.yml'
     @@b1 = Book.new('Book1')
     @@p1 = Person.new
@@ -43,7 +22,7 @@ class MyUtilitiesTest < Test::Unit::TestCase
     assert_nil(My_utilities.convert_to_yaml(save_file, @@ml))
   end
 
-  def test_convert_from_yaml
+  def test_2_convert_from_yaml
     get_file = '../../saveyaml.yml'
     assert_instance_of(My_library, My_utilities.convert_from_yaml(get_file))
 
